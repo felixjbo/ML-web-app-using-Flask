3f9a3289e1e64aeef47466cd13a7a3e2cb5ae0c8
Félix Jiménez Boza <106713963+felixjbo@users.noreply.github.com>
2023-11-20 05:22:25 +0000

    Tests

--- src/app.py
+++ src/app.py
@@ -1,23 +1,30 @@
-from pickle import load
 from flask import Flask, request, render_template
+from pickle import load
+import pandas as pd
 
 app = Flask(__name__)
-model = load(open("/workspaces/ML-web-app-using-Flask/models/NLP_default_42.sav", "rb"))
-class_dict = {
-    "0": "Not Spam",
-    "1": "Spam"
-}
+model = load(open("../models/relu_adam_42.pkl", "rb"))
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
         
-        val1 = float(request.form["val1"])
-        
-        data = [[val1]]
+        HighBP = float(request.form["HighBP"])
+        HighChol = str(request.form["HighChol"])
+        BMI = str(request.form["BMI"])
+        Smoker = str(request.form["Smoker"])
+        Stroke = float(request.form["Stroke"])
+        Diabetes = float(request.form["Diabetes"])
+        PhysActivity = float(request.form["PhysActivity"])
+        HvyAlcoholConsump = float(request.form["HvyAlcoholConsump"])
+        Sex = float(request.form["Sex"])
+        colums = ['HighBP','HighChol','BMI','Smoker','Stroke','Diabetes','PhysActivity','HvyAlcoholConsump','Sex']
+
+        data = pd.DataFrame([[HighBP, HighChol, BMI, Smoker, Stroke, Diabetes, PhysActivity, HvyAlcoholConsump, Sex]], columns=colums)
+        print(data)
         prediction = str(model.predict(data)[0])
-        pred_class = class_dict[prediction]
+        print(prediction)
     else:
-        pred_class = None
+        prediction = None
     
-    return render_template("index.html", prediction = pred_class)
+    return render_template("index.html", prediction = prediction)
